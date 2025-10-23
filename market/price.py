2e05e931ff7756b75ff9b9117f17c21fe4fdb1ef
왕사장님 <@.com>
2025-10-23 13:40:05 +0900

    market: add price snapshot dummy + test

---
+++ market/price.py
@@ -0,0 +1,12 @@
+﻿def get_snapshot(symbol: str):
+    """
+    아주 단순 스냅샷 더미:
+    실제로는 시세 어댑터에서 받아오는 값들을 넣게 됩니다.
+    """
+    return {
+        "symbol": symbol,
+        "price": 1000.0,
+        "volume": 0,
+        "fast": 0.0,  # TA용 자리
+        "slow": 0.0,  # TA용 자리
+    }
---
+++ test_market_price.py
@@ -0,0 +1,3 @@
+﻿from market.price import get_snapshot
+snap = get_snapshot("AAA")
+print("symbol:", snap.get("symbol"), "price:", snap.get("price"))
